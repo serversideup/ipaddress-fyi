657f7f3c5ec0c7fa3fa3236710f260154b7e97a7
Dan Pastori <dan@521dimensions.com>
2017-02-24 16:17:21 -0600

    Changed to 18

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('update:ipaddresses all')
-                  ->dailyAt('22:20');
+                  ->dailyAt('22:18');
     }
 
     /**
