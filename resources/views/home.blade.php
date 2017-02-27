938e188f5fded6d13d7ea8f0163732555eb8ad77
Jay Rogers <jay@521dimensions.com>
2017-02-27 15:52:02 -0600

    Added Timezone on when it was updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,7 +32,7 @@
 											<img src="/img/logos/{{ $ipAddress->app_name }}.svg"/>
 											<div class="service-meta">
 												<span class="service-name">{{ $ipAddress->clean_name }}</span>
-												<span class="last-updated">Last Updated: {{ date('m/d/Y g:i:s A', strtotime( $ipAddress->last_updated ) ) }}</span>
+												<span class="last-updated">Last Updated: {{ date('m/d/Y g:i:s A', strtotime( $ipAddress->last_updated ) ) }} GMT</span>
 											</div>
 										</div>
 										<div style="clear: both;"></div>
